f3a14e4ced499d6020ca3ef79c06a9c93cc2338f
Your Name <ridoferdian3@gmail.com>
2024-08-18 03:50:50 +0700

    kelas

--- dist/js/script.js
+++ dist/js/script.js
@@ -82,7 +82,7 @@ jadwal.addEventListener('click', function() {
 })
 
 
-  var TrandingSlider = new Swiper('.swiper', {
+  var galeriSwiper = new Swiper('.swiper-galeri', {
     effect: 'coverflow',
     grabCursor: true,
     centeredSlides: true,
@@ -94,10 +94,28 @@ jadwal.addEventListener('click', function() {
       depth: 100,
       modifier: 2.5,
     },
-    on: {
-      click(event) {
-        swiper.slideTo(this.clickedIndex);
-      },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
+
+
+  var galeriSwiper = new Swiper('.slide-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2.5,
     },
     pagination: {
       el: '.swiper-pagination',
